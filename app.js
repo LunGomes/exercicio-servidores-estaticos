9fd58e3ef37214d1ddff90628f0a37edb9687331
LunGomes <luanagomessilva112806@gmail.com>
2025-09-04 11:47:04 -0300

    Exibindo o conteúdo do json na página html

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 let button = document.getElementById('btnUsers');
+let div = document.getElementById('out');
 
 button.addEventListener('click', function () {
     // Assincrona Async Chaining
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    fetch('https://jsonplaceholder.typicode.com/posts')
     .then(function (response) {
         return response.json();
     })
-    .then(function (json) {
-        console.log(json);
-        
-    })
-    // console.log("Clicou!");
+    .then(function (data) {
+        div.textContent = `Title: ${data[0].title}, Body: ${data[0].body}`;
+        let listaOrdenada = "<ol>";
+
+        for (usuario in data) {
+            // "<li>" + `Title: ${usuario.title}` + "</li>" ;
+            // listaOrdenada =
+        }
+    });
 });
 
 // Sincrona 
