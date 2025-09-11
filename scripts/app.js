ed08e212731eb532c23f22567af89d7f2de82231
LunGomes <luanagomessilva112806@gmail.com>
2025-09-11 08:42:35 -0300

    Adicionando a classe

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,16 @@
 let button = document.getElementById('btnUsers');
+let div = document.getElementById('out');
 
 button.addEventListener('click', function () {
     // Assincrona
     fetch('https://jsonplaceholder.typicode.com/posts/1')
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+        .then( function (response) {
+            return response.json();
+        })
+        .then( function (json) {
+            div.innerText = json.body;
+            // div.innerText = JSON.stringify(json, null, 0);
+        });
     // console.log("Clicou!");
 });
 
